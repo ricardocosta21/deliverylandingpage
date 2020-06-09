238d20d92883d7aebf918d43e23bf0dff7c12168
Ricardo Costa <ricardo21costa@hotmail.com>
2020-06-09 23:26:09 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Router, Route, Switch } from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 
-// pages for this product
+// pages for this product. 
 import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
